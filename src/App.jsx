06ccb7da17800ee90cdb24ebc47bfe3133c4abfb
Roshan Adithya Jegadees <roshanadithya965@gmail.com>
2026-03-20 02:45:31 +0200

    refactor: removed lazy from Product detail- landing page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
 import './App.css'
 
-const ProductCatalogue = lazy(() => import('./pages/ProductCatalogue/ProductCatalogue'))
+import ProductCatalogue from './pages/ProductCatalogue/ProductCatalogue'
+
 const ProductDetail = lazy(() => import('./pages/ProductDetail/ProductDetail'))
 const Analytics = lazy(() => import('./pages/Analytics/Analytics'))
 const Saved = lazy(() => import('./pages/Saved/Saved'))
